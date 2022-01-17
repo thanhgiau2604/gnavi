1bb57a7c741e664c04d8e4ac1fbeca83b5e3a57f
thanh.dinh <dinhcongthanh1910@gmail.com>
2022-01-17 11:26:35 +0700

    chore: config airbnb and rule

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from 'app/store'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
