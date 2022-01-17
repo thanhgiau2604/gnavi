f2af7f7423cc09b43e0b3f751b87d55926fdb015
thanh.dinh <dinhcongthanh1910@gmail.com>
2022-01-17 11:34:16 +0700

    chore: clean

--- src/app/api/authAPI.ts
+++ src/app/api/authAPI.ts
@@ -3,7 +3,7 @@ import { LoginPayload } from 'interfaces/auth'
 import axiosClient from './axiosClient'
 
 export const authApi = {
-  //example
+  // example
   login(payload: LoginPayload) {
     return axiosClient.post(routes.LOGIN, payload)
   },
