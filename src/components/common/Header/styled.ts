46a0efc41a3b43566df85355c87221496f317704
giau.nguyen <giau.nguyen@executionlab.asia>
2022-01-17 16:39:39 +0700

    refactor: fix style image

--- src/components/common/Header/styled.ts
+++ src/components/common/Header/styled.ts
@@ -24,7 +24,7 @@ const HeaderContainer = styled.header<HeaderProps>`
     transform: translateY(-50%);
   }
 
-  .btn-authen {
+  .authen-btn {
     margin-left: auto;
     display: flex;
     gap: 0 11px;
