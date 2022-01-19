216a39854e63882d5bfdcd610c7ab74b461d4c82
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-01-19 18:44:53 +0700

    fix: apply chartjs instead of echarts

--- src/components/common/RadarChart/index.tsx
+++ src/components/common/RadarChart/index.tsx
@@ -1,72 +1,42 @@
 import React from 'react'
-import ReactECharts from 'echarts-for-react'
-import { Variables } from 'styles/styled/app/Variables'
+import 'chart.js/auto' // required
+import { Radar } from 'react-chartjs-2'
 
 const RadarChart = () => {
-  const option = {
+  const options = {
+    responsive: true,
     legend: {
-      data: ['ガイドスキル'],
-      itemStyle: {
-        color: '#ffb1c1',
-        borderColor: '#ee94a6',
-      },
+      position: 'top',
     },
-    radar: {
-      axisName: {
-        color: '#666666',
-        fontSize: 8,
-      }, // style of label
-      // center: ['50%', '50%'], // center chart
-      splitArea: {
-        areaStyle: {
-          color: [
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-            Variables.colorWhite,
-          ],
-        },
-      }, // style of background chart area
-      nameGap: 5, // distance between the indicator's name and axis
-      splitNumber: 10, // number of levels of the chart
-      indicator: [
-        { name: 'Communication', max: 6500 },
-        { name: 'Mindset', max: 16000 },
-        { name: 'Knowledge', max: 30000 },
-        { name: 'Technique', max: 38000 },
-        { name: 'Basic Capabilities', max: 52000 },
-      ], // label and value of each data
-    },
-    series: [
+    scale: {
+      reverse: false,
+      max: 10,
+      min: 0,
+      stepSize: 1,
+    }, // number of levels of the chart
+  }
+  const data = {
+    labels: ['Communication', 'Basic Capabilities', 'Technique', 'Knowledge', 'Mindset'],
+    datasets: [
       {
-        type: 'radar',
-        data: [
-          {
-            value: [4200, 3000, 20000, 35000, 50000],
-            name: 'ガイドスキル',
-            areaStyle: {
-              color: '#ffb1c1', // color of area charts
-            },
-            lineStyle: {
-              color: '#ee94a6', // color of lines
-              width: 1.5,
-            },
-            itemStyle: {
-              color: 'transparent', // color of data points
-              borderWidth: 1.5,
-            },
-          },
-        ],
+        label: 'ガイドスキル',
+        data: [8, 6, 10, 7, 9], // indicators
+        /* styles */
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)',
+        /* styles */
       },
     ],
   }
-  return <ReactECharts option={option} />
+  return (
+    <div>
+      <Radar data={data} height={50} width={50} options={options} />
+    </div>
+  )
 }
 
 export default RadarChart
