203f66b9908913f3332628866cdd6db44f310727
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-01-23 14:33:11 +0700

    chore: use next/link with ScheduleBlock

--- src/components/layout/Schedule/Block/index.tsx
+++ src/components/layout/Schedule/Block/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FlexContainer from 'styles/styled/layout/FlexLayout'
 import Image from 'next/image'
+import Link from 'next/link'
 import { ScheduleBlockContainer } from './styled'
 
 interface ScheduleBlockProps {
@@ -9,27 +10,29 @@ interface ScheduleBlockProps {
 
 const ScheduleBlock: React.FC<ScheduleBlockProps> = ({ data }) => {
   return (
-    <ScheduleBlockContainer>
-      <FlexContainer position="relative">
-        <p className="title">{data.title}</p>
-        <div className="btn-more">
-          <Image src="/images/more.png" alt="" width={16} height={4} objectFit="contain" />
-        </div>
-      </FlexContainer>
-      {data.image && (
-        <div className="image-container">
-          <Image src={data.image} alt="" layout="fill" objectFit="cover" priority />
-        </div>
-      )}
+    <Link href="#">
+      <ScheduleBlockContainer>
+        <FlexContainer position="relative">
+          <p className="title">{data.title}</p>
+          <div className="btn-more">
+            <Image src="/images/more.png" alt="" width={16} height={4} objectFit="contain" />
+          </div>
+        </FlexContainer>
+        {data.image && (
+          <div className="image-container">
+            <Image src={data.image} alt="" layout="fill" objectFit="cover" priority />
+          </div>
+        )}
 
-      <p className="content">
-        西表島では仲間川マングローブクルーズにご乗船。水牛車で由布島に渡り、亜熱帯特有の動植物を観察できる由布島植物……
-      </p>
-      {/* Will replace soon */}
-      <div className="btn btn-icon">
-        <Image src="/images/like.png" layout="fill" objectFit="contain" />
-      </div>
-    </ScheduleBlockContainer>
+        <p className="content">
+          西表島では仲間川マングローブクルーズにご乗船。水牛車で由布島に渡り、亜熱帯特有の動植物を観察できる由布島植物……
+        </p>
+        {/* Will replace soon */}
+        <div className="btn btn-icon">
+          <Image src="/images/like.png" layout="fill" objectFit="contain" />
+        </div>
+      </ScheduleBlockContainer>
+    </Link>
   )
 }
 
--- src/pages/tourplan/index.tsx
+++ src/pages/tourplan/index.tsx
@@ -6,9 +6,11 @@ import Schedule from 'components/layout/Schedule'
 const TourPlan = () => {
   const data = [
     {
+      id: 1,
       year: '2021年',
       schedules: [
         {
+          id: 1,
           datetime: '12月15日',
           title: '西表・由布・竹富島めぐり日帰りツアー',
           image: '',
@@ -16,6 +18,7 @@ const TourPlan = () => {
             '西表島では仲間川マングローブクルーズにご乗船。水牛車で由布島に渡り、亜熱帯特有の動植物を観察できる由布島植物……',
         },
         {
+          id: 2,
           datetime: '12月12日',
           title: 'マンタとウミガメシュノーケリング+幻の島上陸ツアー',
           image: '/images/schedule01.png',
@@ -23,6 +26,7 @@ const TourPlan = () => {
             '西表島の大自然を満喫する凝縮ツアー！SUP（またはカヌー）で”幸運の滝”クーラの滝へ！滝遊びと鍾乳洞探検を楽し……',
         },
         {
+          id: 3,
           datetime: '12月10日',
           title: 'マングローブSUP&バラス島上陸',
           image: '/images/schedule02.png',
