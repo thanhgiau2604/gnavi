5e6a727ee7c00b5c936876d50a28c424165cb4b8
giau.nguyen <giau.nguyen@executionlab.asia>
2022-01-24 20:30:08 +0700

    chore: change name func

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -7,14 +7,14 @@ import { HeaderContainer, HeaderTitle } from './styled'
 
 const Header = ({ title, showBtnBack, shadow, backTo, children }: HeaderProps) => {
   const router = useRouter()
-  const handleBackScreen = () => backTo && router.push(backTo)
+  const handleNavigateBack = () => backTo && router.push(backTo)
 
   return (
     <div className="wrapper">
       <HeaderContainer shadow={shadow}>
         <div className={showBtnBack ? 'back-btn' : ''}>
           {showBtnBack ? (
-            <ImageContainer width={24} height={24} onClick={handleBackScreen}>
+            <ImageContainer width={24} height={24} onClick={handleNavigateBack}>
               <Image src="/images/arrow_back.png" alt="" layout="fill" objectFit="contain" />
             </ImageContainer>
           ) : (
