fbf3fad25bfdee47878a9bb7732c2cc22c13befb
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-01-26 15:08:04 +0700

    chore: remove duplicate values

--- src/components/common/ButtonReaction/styled.ts
+++ src/components/common/ButtonReaction/styled.ts
@@ -13,7 +13,6 @@ const ButtonReactionContainer = styled.div<ButtonReactionCotaninerProps>`
   cursor: pointer;
   position: ${({ position }) => position && position};
   top: ${({ top }) => top && top};
-  top: ${({ left }) => left && left};
   left: ${({ left }) => left && left};
   right: ${({ right }) => right && right};
   bottom: ${({ bottom }) => bottom && bottom};
