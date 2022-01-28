8ff5a2a4219b2d6d7b3458a87ba33622c8c288c4
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-01-28 15:44:44 +0700

    update TourPlan/index

--- src/containers/TourPlan/index.tsx
+++ src/containers/TourPlan/index.tsx
@@ -4,8 +4,8 @@ import { TourLogProps, TourPlanProps } from 'interfaces/TourLog'
 import { Container } from 'styles/styled/layout/ContainerLayout'
 import Header from 'components/common/Header'
 import Button from 'components/common/Button'
-import TourLog from 'components/layout/TourPlan/TourLog'
 import { TourPlanContainer } from './styled'
+import TourLog from 'components/layout/TourPlan/TourLog'
 
 const TourPlan = () => {
   // const data: TourPlanProps[] = []
