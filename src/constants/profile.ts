686186c5257331faa10e1d0c7fe87ec516e919c9
thanh.dinh <dinhcongthanh1910@gmail.com>
2022-02-01 13:44:36 +0700

    fix: change require input page edit profile (happy new year)

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -17,18 +17,18 @@ export const INIT_FORM_EDIT_PROFILE_VALUE = {
   public_setting_email_contact: 'yourself',
 }
 export const FORM_EDIT_PROFILE_VALIDATE_SCHEMA = Yup.object().shape({
-  nickname: Yup.string().required(),
+  nickname: Yup.string(),
   email: Yup.string().required(),
   password: Yup.string().required(),
   first_name: Yup.string().required(),
   last_name: Yup.string().required(),
   public_setting_full_name: Yup.string().required(),
-  eng_name: Yup.string().required(),
+  eng_name: Yup.string(),
   birthday: Yup.date().required(),
   gender: Yup.string().required(),
-  chua_biet: Yup.string().required(),
-  phone_number: Yup.string().required(),
+  chua_biet: Yup.string(),
+  phone_number: Yup.string(),
   public_setting_phone_number: Yup.string().required(),
-  email_contact: Yup.string().required(),
+  email_contact: Yup.string(),
   public_setting_email_contact: Yup.string().required(),
 })
