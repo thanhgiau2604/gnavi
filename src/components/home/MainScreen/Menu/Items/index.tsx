64579bb7d7c81555e0da2800815caf3fb626679b
huongtran1413 <huong.tran@executionlab.asia>
2022-02-07 11:18:06 +0700

    fix: upload file home screen ui, add redirecto

--- src/components/home/MainScreen/Menu/Items/index.tsx
+++ src/components/home/MainScreen/Menu/Items/index.tsx
@@ -2,19 +2,21 @@ import type { FC } from 'react'
 import { ImageContainer } from 'styles/styled/app/Image'
 import Image from 'next/image'
 import FlexContainer from 'styles/styled/layout/FlexLayout'
+import Link from 'next/link'
 import { MenuItemContainer } from './styled'
 
 interface MenuItemsProps {
   option: string
+  redirectTo: string
 }
 
-const MenuItems: FC<MenuItemsProps> = ({ option }) => {
+const MenuItems: FC<MenuItemsProps> = ({ option, redirectTo }) => {
   return (
     <MenuItemContainer>
       <FlexContainer justifyContent="space-between" className="container item-container">
-        <a href="#" className="text">
-          {option}
-        </a>
+        <Link href={redirectTo}>
+          <a className="text">{option}</a>
+        </Link>
         <div className="info-guide-button">
           <ImageContainer width={12} height={20}>
             <Image src="/images/arrow_right.png" alt="Picture" layout="fill" objectFit="contain" />
--- src/components/home/MainScreen/Menu/index.tsx
+++ src/components/home/MainScreen/Menu/index.tsx
@@ -5,6 +5,7 @@ import { HomeMenuContainer } from './styled'
 interface ITEM {
   label: string
   option: string[]
+  redirectTo: string[]
 }
 
 const Menu: FC = () => {
@@ -12,22 +13,27 @@ const Menu: FC = () => {
     {
       label: '',
       option: ['スキルチェック', 'ツアープラン一覧', 'ツアーログ'],
+      redirectTo: ['#/1', '#/2', '#/3'],
     },
     {
       label: 'お役立ち',
       option: ['ガイドの知恵袋', 'その他'],
+      redirectTo: ['#/4', '#/5'],
     },
     {
       label: '設定',
       option: ['アカウント情報', '決済情報'],
+      redirectTo: ['#/6', '#/7', '#/8'],
     },
     {
       label: '',
       option: ['公開プロフィール検索'],
+      redirectTo: ['#/9'],
     },
     {
       label: '',
       option: ['プライバシーポリシー', '利用規約'],
+      redirectTo: ['#/10', '#/11'],
     },
   ]
   return (
@@ -40,7 +46,11 @@ const Menu: FC = () => {
               {item.label}
             </h5>
           )}
-          {item.option && item.option.map((o: string) => <MenuItems key={o} option={o} />)}
+
+          {item.option &&
+            item.option.map((o: string, i: number) => (
+              <MenuItems key={o} option={o} redirectTo={item.redirectTo[i]} />
+            ))}
           <div className="line-bottom" />
         </>
       ))}
