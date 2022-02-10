551f71f9f92f2e1004ac4a2a93e61a6be1c8d31a
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-02-10 15:53:24 +0700

    refactor: init constant for tour-plan form values

--- src/containers/TourPlan/Create/index.tsx
+++ src/containers/TourPlan/Create/index.tsx
@@ -1,64 +1,13 @@
-import { ROUTES } from '@constants'
+import { INIT_TOUR_PLAN_FORM_VALUE, ROUTES } from '@constants'
 import ButtonGroup, { ButtonGroupItem } from 'components/common/ButtonGroup'
 import Header from 'components/common/Header'
 import FormInfo from 'components/common/TourPlan/FormInfo'
 import { Formik } from 'formik'
-import { TourPlanInfoProps } from 'interfaces/TourPlan'
 import React, { useState } from 'react'
 import { TourPlanCreateContainer } from './styled'
 
 const TourPlanCreate = () => {
   const [isTab, setIsTab] = useState<string>('ツアー情報')
-  const initialValues: TourPlanInfoProps = {
-    // 基本情報 - Basic info
-    tour_name: '',
-    order_date: '',
-    start_date: '',
-    start_time: '',
-    overview: '',
-    order_form: '直接',
-    company_name: '',
-    phone: '',
-    email: '',
-    remuneration: 0,
-    price: 0,
-    payment_date: '',
-    method_payment: '',
-    // ゲスト情報 - Guest info
-    no_of_adults: 0,
-    no_of_teens: 0,
-    no_of_childs: 0,
-    relation: '',
-    diet: '',
-    represent: '',
-    gender: 'male',
-    age: 0,
-    country: '',
-    contact_phone: '',
-    contact_email: '',
-    note: '',
-    // スポット情報 - Plan info
-    tour_start_time: '',
-    tour_end_time: '',
-    meeting_place: '',
-    dissolution_place: '',
-    events: [
-      {
-        id: 1,
-        event_name: '',
-        post_code: '',
-        provinces: '',
-        address1: '',
-        address2: '',
-        phone: '',
-        reservation: false,
-        start_time: '',
-        end_time: '',
-        content: '',
-        transporation: [],
-      },
-    ],
-  }
 
   return (
     <TourPlanCreateContainer>
@@ -81,7 +30,7 @@ const TourPlanCreate = () => {
         />
       </ButtonGroup>
       <Formik
-        initialValues={initialValues}
+        initialValues={INIT_TOUR_PLAN_FORM_VALUE}
         onSubmit={(values, actions) => {
           console.log(values)
           actions.setSubmitting(false)
