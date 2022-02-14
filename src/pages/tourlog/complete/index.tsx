762130a4b6b23b5dfd740debc98316658cea1656
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-02-14 10:49:46 +0700

    feat: build tour-log create complete layout

---
+++ src/pages/tourlog/complete/index.tsx
@@ -0,0 +1,9 @@
+import TourLogCreateComplete from 'containers/TourLog/Complete'
+import { NextPage } from 'next'
+import React from 'react'
+
+const TourPlanCreateCompletePage: NextPage = () => {
+  return <TourLogCreateComplete />
+}
+
+export default TourPlanCreateCompletePage
