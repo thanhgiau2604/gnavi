21ad5e0d0beea65a7b5412bbc66905db585c550d
thanh.dinh <dinhcongthanh1910@gmail.com>
2022-02-14 13:52:46 +0700

    feat: update group btn tab 2nd by design 14.2.22

--- src/components/common/ButtonGroup/styled.ts
+++ src/components/common/ButtonGroup/styled.ts
@@ -2,21 +2,30 @@ import styled, { css } from 'styled-components'
 import { Variables } from 'styles/styled/app/Variables'
 
 interface props {
+  rowTop?: boolean
+
   active?: boolean
 }
 
-export const ButtonGroupStyledContainer = styled.table`
+export const ButtonGroupStyledContainer = styled.table<props>`
   border-collapse: collapse;
   width: 100%;
+  td {
+    padding: 0;
+    ${({ rowTop }) => rowTop === false && `border-top: none !important`};
+  }
 `
 
 export const ButtonGroupStyledItem = styled.td<props>`
   border: 1px solid ${Variables.color3};
   text-align: center;
-  padding: 5px;
   color: ${Variables.colorPrimary};
   cursor: pointer;
   width: 1px;
+
+  .text {
+    padding: 6px;
+  }
 
   ${({ active }) =>
     active &&
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,4 @@
 export function removeFontFamily(stringRaw: string) {
+  // react-draft-wysiwyg if input width japan language will add font-family but func parser (lib html-react-parser) not support
   return stringRaw.replace(`font-family: Noto Sans JP", sans-serif;`, '')
 }
