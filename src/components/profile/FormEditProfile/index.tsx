91ab69a6dc77ac1c8e2dfc5c883418ea446016f0
thanh.dinh <dinhcongthanh1910@gmail.com>
2022-02-16 09:41:49 +0700

    feat: update page edit info profile

--- src/components/profile/FormEditProfile/index.tsx
+++ src/components/profile/FormEditProfile/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FastField, Form, Formik } from 'formik'
+import { ColorResult } from 'react-color'
 import { EditProfileFormProps } from 'interfaces/Profile'
 import Button from 'components/common/Button'
+import ButtonUpload from 'components/common/ButtonUpload'
 import CustomInput from 'components/common/CustomFields/InputField'
 import CustomSelect from 'components/common/CustomFields/SelectField'
 import CustomDate from 'components/common/CustomFields/DateField'
+import ColorPicker from 'components/ColorPicker'
 import {
   BUTTON_COLORS,
+  BUTTON_UPLOAD_COLORS,
+  FONT_SIZES,
   OPTIONS_GENDER,
   OPTIONS_PUBLIC_SETTING,
   FORM_EDIT_PROFILE_VALIDATE_SCHEMA,
   INIT_FORM_EDIT_PROFILE_VALUE,
+  InitColorResult,
 } from '@constants'
+import { FieldLabel } from 'components/common/CustomFields/styled'
 
 const EditProfileForm: React.FC<EditProfileFormProps> = ({ handleSubmit }) => {
+  const [showColorPicker, setShowColorPicker] = useState<boolean>(false)
+  const [color, setColor] = useState<string>('#fff')
+  const [colorResult, setColorResult] = useState<ColorResult>(InitColorResult)
+
+  const handleShowColorPicker = () => setShowColorPicker(!showColorPicker)
+  const handleChangeColor = (_color: string) => setColor(_color)
+  const handleChangeColorResult = (_colorResult: ColorResult) => setColorResult(_colorResult)
   return (
     <Formik
       initialValues={INIT_FORM_EDIT_PROFILE_VALUE}
@@ -27,20 +41,12 @@ const EditProfileForm: React.FC<EditProfileFormProps> = ({ handleSubmit }) => {
         return (
           <Form>
             <FastField
-              name="nickname"
-              component={CustomInput}
-              label="ニックネーム"
-              lbTag="any"
-              pb={1}
-            />
-            <p className="profile-form-edit-sub-text-input-nickname">後から変更できます</p>
-
-            <FastField
               name="email"
               component={CustomInput}
               label="メールアドレス"
               lbTag="require"
             />
+
             <FastField
               name="password"
               component={CustomInput}
@@ -48,31 +54,18 @@ const EditProfileForm: React.FC<EditProfileFormProps> = ({ handleSubmit }) => {
               lbTag="require"
               type="password"
             />
-            <FastField
-              name="first_name"
-              component={CustomInput}
-              label="本名 (苗字)"
-              lbTag="require"
-            />
-            <FastField
-              name="last_name"
-              component={CustomInput}
-              label="本名 (名前)"
-              lbTag="require"
-            />
-            <FastField
-              name="public_setting_full_name"
-              component={CustomSelect}
-              label="公開設定(本名)"
-              options={OPTIONS_PUBLIC_SETTING}
-            />
+
+            <FastField name="name" component={CustomInput} label="名前" lbTag="require" />
+
             <FastField
               name="eng_name"
               component={CustomInput}
               label="名前(英語/ローマ字表記)"
               lbTag="any"
             />
+
             <FastField name="birthday" component={CustomDate} label="誕生日" lbTag="require" />
+
             <FastField
               name="gender"
               component={CustomSelect}
@@ -80,9 +73,22 @@ const EditProfileForm: React.FC<EditProfileFormProps> = ({ handleSubmit }) => {
               lbTag="require"
               options={OPTIONS_GENDER}
             />
+
             <FastField name="membership" component={CustomInput} label="所属団体" lbTag="any" />
 
-            <FastField name="phone_number" component={CustomInput} label="所属団体" lbTag="any" />
+            <Button
+              width="100%"
+              buttonColor={BUTTON_COLORS.style06}
+              title="所属団体を追加"
+              margin="0 0 30px 0"
+            />
+
+            <FastField
+              name="phone_number"
+              component={CustomInput}
+              label="連絡先電話番号"
+              lbTag="any"
+            />
 
             <FastField
               name="public_setting_phone_number"
@@ -90,24 +96,57 @@ const EditProfileForm: React.FC<EditProfileFormProps> = ({ handleSubmit }) => {
               label="公開設定 (連絡先電話番号)"
               options={OPTIONS_PUBLIC_SETTING}
             />
+
             <FastField
               name="email_contact"
               component={CustomInput}
               label="連絡先メールアドレス"
               lbTag="any"
             />
+
             <FastField
               name="public_setting_email_contact"
               component={CustomSelect}
               label="公開設定 (連絡先メールアドレス)"
               options={OPTIONS_PUBLIC_SETTING}
             />
+
+            <FieldLabel>
+              <label className="profile-label">カバー画像を設定</label>
+              <p className="lb-tag lb-tag--any">任意</p>
+            </FieldLabel>
+
+            <ButtonUpload
+              buttonUploadColor={BUTTON_UPLOAD_COLORS.style02}
+              _fontSize={FONT_SIZES.small}
+              fontWeight={400}
+              title="画像を選択する"
+              margin="0 0 3.6rem 0"
+            />
+
+            <FieldLabel>
+              <label className="profile-label">プロフィールのテーマカラーを設定</label>
+              <p className="lb-tag lb-tag--any">任意</p>
+            </FieldLabel>
+
+            <button type="button" className="color-picker-input" onClick={handleShowColorPicker}>
+              {color}
+            </button>
+
+            {showColorPicker && (
+              <ColorPicker
+                color={colorResult}
+                handleChangeColor={handleChangeColor}
+                handleChangeColorResult={handleChangeColorResult}
+              />
+            )}
+
             <Button
               title="保存"
               type="submit"
               buttonColor={BUTTON_COLORS.style02}
               height={48}
-              margin="4rem 0 2rem 0"
+              margin="2rem 0 2rem 0"
             />
           </Form>
         )
--- src/constants/color_picker.ts
+++ src/constants/color_picker.ts
@@ -1,5 +1,5 @@
 export const InitColorResult = {
-  hex: '#000',
+  hex: '#FFFFFF',
   rgb: { r: 51, g: 51, b: 51, a: 1 },
   hsl: { h: 0, s: 0, l: 0.2, a: 1 },
 }
