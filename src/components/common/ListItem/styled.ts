93f861002b7ad8e7c7b182a909695864672d5333
giau.nguyen <giau.nguyen@executionlab.asia>
2022-02-17 13:31:11 +0700

    refactor: remove duplicate

--- src/components/common/ListItem/styled.ts
+++ src/components/common/ListItem/styled.ts
@@ -6,8 +6,6 @@ const ListItemContainer = styled.div<ListItemStyleProps>`
   width: 100%;
   background-color: ${Variables.colorWhite};
   font-size: ${({ fontSize }) => fontSize && `calc(${fontSize}rem / 10)`};
-  border-left: 1px solid ${Variables.color3};
-  border-right: 1px solid ${Variables.color3};
   border-top: ${({ borderTop }) => (borderTop ? `1px solid ${Variables.color3}` : `none`)};
   border-bottom: ${({ borderBottom }) => (borderBottom ? `1px solid ${Variables.color3}` : `none`)};
   border-left: 1px solid ${Variables.color3};
