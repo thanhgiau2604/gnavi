fdb01d380c57eb6b34bc88893170e63ae2c82514
giau.nguyen <giau.nguyen@executionlab.asia>
2022-02-22 15:29:47 +0700

    feat: policies, terms of service page

---
+++ src/pages/policies/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { NextApplicationPage } from 'pages/_app'
+import PolicesContainer from 'containers/Policies'
+
+const PoliciesPage: NextApplicationPage = () => {
+  return <PolicesContainer />
+}
+
+export default PoliciesPage
---
+++ src/pages/terms_of_service/index.tsx
@@ -0,0 +1,9 @@
+import TermsServiceContainer from 'containers/TermsService'
+import { NextApplicationPage } from 'pages/_app'
+import React from 'react'
+
+const TermsServicePage: NextApplicationPage = () => {
+  return <TermsServiceContainer />
+}
+
+export default TermsServicePage
