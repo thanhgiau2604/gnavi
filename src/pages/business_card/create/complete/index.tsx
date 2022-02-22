6f8de791641a73a5ba9c357610151a5992373693
Nghiep Hong <nghiep.hong@executionlab.asia>
2022-02-22 15:32:43 +0700

    feat: build layout business-card create complete

---
+++ src/pages/business_card/create/complete/index.tsx
@@ -0,0 +1,9 @@
+import BusinessCardCreateComplete from 'containers/BusinessCard/Create/Complete'
+import { NextApplicationPage } from 'pages/_app'
+import React from 'react'
+
+const BusinessCardCreateCompletePage: NextApplicationPage = () => {
+  return <BusinessCardCreateComplete />
+}
+
+export default BusinessCardCreateCompletePage
